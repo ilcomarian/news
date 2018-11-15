b1d1ca710a4f8197a8121f779aaf2221443b4fbe
ilcomarian <ilcomarian@gmail.com>
2018-11-15 18:07:19 -0500

    create home routs news nav and sports

---
+++ src/components/nav.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class NavBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default NavBar;
