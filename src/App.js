c4b18e1c2d4780c363e44177e1ae0e3391a1014b
ilcomarian <ilcomarian@gmail.com>
2018-11-15 18:04:28 -0500

    create react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Layout,
+  Header,
+  Textfield,
+  HeaderRow,
+  Navigation,
+  Drawer,
+  Content
+} from "react-mdl";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="demo-big-content">
+          <Layout>
+            <Header waterfall>
+              <HeaderRow title="Title">
+                <Textfield
+                  value=""
+                  onChange={() => {}}
+                  label="Search2"
+                  expandable
+                  expandableIcon="search"
+                />
+              </HeaderRow>
+              <HeaderRow>
+                <Navigation>
+                  <a href="#">Link</a>
+                  <a href="#">Link</a>
+                  <a href="#">Link</a>
+                  <a href="#">Link</a>
+                </Navigation>
+              </HeaderRow>
+            </Header>
+            <Drawer title="Title">
+              <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+              </Navigation>
+            </Drawer>
+            <Content>
+              <div className="page-content" />
+            </Content>
+          </Layout>
+        </div>
       </div>
     );
   }
